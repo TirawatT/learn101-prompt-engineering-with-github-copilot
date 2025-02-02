2391e0832c6e17bf6d5635048b6eb5934bf6906f
tirawatt <tirawat.thongsakun@gmail.com>
2025-02-02 20:30:58 +0700

    promp gen doc

--- 001-TestGitHubCopilot/python-project/demo3.py
+++ 001-TestGitHubCopilot/python-project/demo3.py
@@ -4,9 +4,19 @@ def string_to_array(string):
 
 # func array string to string
 def array_to_string(array, separate=' '):
+    """
+    Convert an array of strings into a single string with a specified separator.
+
+    Parameters:
+    array (list): The list of strings to join.
+    separate (str): The separator to use between elements. Default is a space.
+
+    Returns:
+    str: The joined string.
+    """
     return separate.join(array)
 
 txt = "hello world"
 print(txt)
 print(string_to_array(txt))
-print(array_to_string(string_to_array(txt), '-'))   
+print(array_to_string(string_to_array(txt), '-'))
